040cd5bd342e28305b90179743af610712e41a6a
Leia <leiahopf@googlemail.com>
2020-08-10 14:58:43 +0100

    Fix: Mission ID to Class. Group-Info + sep IDs

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ var slideLeft = {
     distance: '20%',
     origin: 'left',
     opacity: 0,
-    duration: 800,
+    duration: 900,
     easing: 'ease',
     scale: .75,
 };
@@ -12,15 +12,15 @@ var slideRight = {
     distance: '20%',
     origin: 'right',
     opacity: 0,
-    duration: 800,
+    duration: 900,
     easing: 'ease',
     scale: .75,
 };
 
-ScrollReveal().reveal('.quote-reveal1',  { ...slideLeft, delay: 400, });
-ScrollReveal().reveal('.quote-reveal2',  { ...slideRight, delay: 800, });
-ScrollReveal().reveal('.quote-reveal3',  { ...slideLeft, delay: 1200, });
-ScrollReveal().reveal('.quote-reveal4',  { ...slideRight, delay: 1600, });
-ScrollReveal().reveal('.quote-reveal5',  { ...slideLeft, delay: 2000, });
-ScrollReveal().reveal('.quote-reveal6',  { ...slideRight, delay: 2400, });
-ScrollReveal().reveal('.quote-reveal7',  { ...slideLeft, delay: 2800, });
+ScrollReveal().reveal('.quote-reveal1',  { ...slideLeft, delay: 300, });
+ScrollReveal().reveal('.quote-reveal2',  { ...slideRight, delay: 600, });
+ScrollReveal().reveal('.quote-reveal3',  { ...slideLeft, delay: 900, });
+ScrollReveal().reveal('.quote-reveal4',  { ...slideRight, delay: 1200, });
+ScrollReveal().reveal('.quote-reveal5',  { ...slideLeft, delay: 1500, });
+ScrollReveal().reveal('.quote-reveal6',  { ...slideRight, delay: 1800, });
+ScrollReveal().reveal('.quote-reveal7',  { ...slideLeft, delay: 2100, });
